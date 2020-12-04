16c910dba01989bd40fac75e1b60e87f1742a8fe
pextech <mcstain1639@gmail.com>
2020-12-04 19:35:52 +0200

    working progress

--- server.js
+++ server.js
@@ -1,51 +1,14 @@
-
-require('babel-register');
-
+import {} from 'dotenv/config';
 import express from 'express';
-import morgan from 'morgan';
-import mongoose from 'mongoose';
 
-import contactRoute from './src/routes/contactRoute';
+const app = express();
 
-export const app = express();
+app.use(express.json());
 
-app.use(express.json())
+app.get('/', (req, res) => res.status(200).send({ message: 'YAY! Congratulations! Your first endpoint is working' }));
 
-const URI = 'mongodb+srv://pextech:Mc1639_1639@cluster0.fyxsl.mongodb.net/mybrand?retryWrites=true&w=majority';
-mongoose.connect(URI,{useNewUrlParser: true, useUnifiedTopology: true}).then((result)=>{
-    app.listen(3000);
-
-    
+app.listen(process.env.PORT, () => {
+  console.log(`app running on port ${process.env.PORT}`);
 });
 
-
-
-app.use(morgan('dev'));
-
-app.use((req,res,next)=>{
-
-    res.header('Access-Control-Allow-Origin','*');
-    res.header('Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type,Accept,Authorization');
-
-    if(req.method ==='OPTIONS'){
-        res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE');
-        return res.status(200).json({});
-    }
-    next();
-})
-
-app.use('/',contactRoute);    
-
-
-app.use((req,res,next)=>{
-
-    const error = new Error('Page Not found');
-       res.status(404).json({
-   
-           error:{
-               message: error.message
-           }
-       });
-   
-   });
+export default app;
