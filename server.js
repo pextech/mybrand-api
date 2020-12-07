bc8ff20dfb59c134aebdb1a37a326a2d5c870822
pextech <mcstain1639@gmail.com>
2020-12-07 10:10:11 +0200

    working progress

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => res.status(200).send({ message: 'YAY! Congratulations! Your first endpoint is working' }));
+app.get('/', (req, res) => res.status(200).send({ message: 'YAY! Congratulations! Your first heroku app  is working' }));
 
 app.listen(process.env.PORT || 5000, () => {
   console.log(`app running on port ${process.env.PORT}`);
