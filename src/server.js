30253177a22f0bce7f3ea87686dc7c9c6db0236c
pextech <mcstain1639@gmail.com>
2020-12-18 00:39:02 +0200

    welcoming

--- src/server.js
+++ src/server.js
@@ -9,7 +9,9 @@ const app = express();
 
 const URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fyxsl.mongodb.net/mybrand?retryWrites=true&w=majority`;
 mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
-  app.listen(process.env.PORT || 5000);
+  app.listen(process.env.PORT || 5000).then((res) => {
+    res.status(200).json('Welcome to my Api , have fun!!');
+  });
 });
 
 app.use(express.json());
@@ -27,9 +29,6 @@ app.use((req, res, next) => {
     return res.status(200).json({});
   }
   next();
-});
-app.use('/', (req, res) => {
-  res.status(200).json({ message: 'Welcome to my APi, have fun!!' });
 });
 app.use('/', blogRoute);
 app.use('/', contactRoute);
