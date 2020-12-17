8032cc0c8c5090050e10ec94d25a5d54be148fce
pextech <mcstain1639@gmail.com>
2020-12-18 00:50:23 +0200

    progress

--- src/server.js
+++ src/server.js
@@ -9,11 +9,8 @@ const app = express();
 
 const URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fyxsl.mongodb.net/mybrand?retryWrites=true&w=majority`;
 mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
-  app.listen(process.env.PORT || 5000).then((res) => {
-    res.status(200).json('Welcome to my Api , have fun!!');
-  });
+  app.listen(process.env.PORT || 5000);
 });
-
 app.use(express.json());
 app.use(express.json({ extended: false }));
 app.use(fileupload({ useTempFiles: true }));
@@ -29,6 +26,9 @@ app.use((req, res, next) => {
     return res.status(200).json({});
   }
   next();
+});
+app.get('/',(req,res) => {
+  res.status(200).json({message: 'Hey, you made it to my API, have fun'});
 });
 app.use('/', blogRoute);
 app.use('/', contactRoute);
